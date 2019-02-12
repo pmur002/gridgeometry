212ee50dbc22865bd3e285fa645f51ac1bee501b
Paul Murrell <paul@stat.auckland.ac.nz>
2019-02-13 11:22:04 +1300

    changed from S4 generic to S3 generic (easier to extend because grobs are S3 classes)

--- R/polyclip.R
+++ R/polyclip.R
@@ -1,33 +1,28 @@
 
-## Make 'grid' S3 classes (including class hierarchy) visible to S4
-setOldClass("grob")
-setOldClass(c("circle", "grob"))
-setOldClass(c("lines", "grob"))
-setOldClass(c("polygon", "grob"))
-setOldClass(c("rect", "grob"))
-setOldClass(c("roundrect", "grob"))
-setOldClass(c("segments", "grob"))
-setOldClass(c("text", "grob"))
-setOldClass(c("xspline", "grob"))
-setOldClass("gList")
-setOldClass(c("gTree", "grob"))
+## Create S3 generic from polyclip::polyclip()
+polyclip <- function(A, B, ...) {
+    UseMethod("polyclip")
+}
 
-## Combine "grob"s and "gList"s so can have one method for both
-setClassUnion("gridgrob", c("grob", "gList"))
+polyclip.default <- function(A, B, ...) {
+    polyclip::polyclip(A, B, ...)
+}
 
-## Create S4 generic from polyclip::polyclip()
-setGeneric("polyclip",
-           function(A, B, ...) {
-               polyclip::polyclip(A, B, ...)
-           })
+polyclipGridGrob <- function(A, B, op, closed, ...) {
+    if (!(inherits(B, "grob") || inherits(B, "gList")))
+        stop("Argument 'B' must be a grob")
+    polyA <- grobCoords(A, closed=closed)
+    polyB <- grobCoords(B, TRUE)
+    polyclip::polyclip(polyA, polyB, op=op, closed=closed, ...)
+}
 
-## Grob methods
-setMethod("polyclip",
-          c("gridgrob", "gridgrob"),
-          function(A, B, op="intersection", closed=TRUE, ...) {
-              polyA <- grobCoords(A, closed=closed)
-              polyB <- grobCoords(B, TRUE)
-              polyclip::polyclip(polyA, polyB, op=op, closed=closed, ...)
-          })
+polyclip.grob <- function(A, B, op="intersection", closed=TRUE, ...) {
+    polyclipGridGrob(A, B, op, closed, ...)
+}
+
+polyclip.gList <- function(A, B, op="intersection", closed=TRUE, ...) {
+    polyclipGridGrob(A, B, op, closed, ...)
+}
+
 
 
