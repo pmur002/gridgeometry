059f72dff9a401efd41ac44e94e554ac6c4eef01
Jack Wong <jackwong199412@hotmail.com.hk>
2022-03-26 16:33:31 +1300

    Add implementation for polyoffset and polylineoffset with corresponding tests

--- R/offset.R
+++ R/offset.R
@@ -1,9 +1,13 @@
-grid.polyoffset <- function()
+grid.polyoffset <- function(A, delta, ..., eps, x0, y0, miterlim = 2, arctol = abs(delta) / 100, jointype = c("square", "round", "miter"))
 {
-  
+  polyA <- grobCoords(A, closed = TRUE)
+  coords <- polyclip::polyoffset(polyA, delta = delta, ..., eps = eps, x0 = x0, y0 = y0, miterlim = miterlim, arctol = arctol, jointype = jointype)
+  return (xyListPolygon(coords))
 }
 
-grid.polylineoffset <- function()
+grid.polylineoffset <- function(A, delta, ..., eps, x0, y0, miterlim = 2, arctol = abs(delta) / 100, jointype = c("square", "round", "miter"), endtype = c("closedpolygon", "closedline", "openbutt", "opensquare", "openround", "closed", "butt", "square", "round"))
 {
-  
+  polyA <- grobCoords(A, closed = F)
+  coords <- polyclip::polylineoffset(polyA, delta = delta, ..., eps = eps, x0 = x0, y0 = y0, miterlim = miterlim, arctol = arctol, jointype = jointype, endtype = endtype)
+  return (xyListPolygon(coords))
 }
--- tests/test2.R
+++ tests/test2.R
@@ -8,3 +8,26 @@ stopifnot(identical(nullGrob(name = "2"), emptyGrob2))
 
 emptyGrob3 = xyListPolygon(x, name = 3)
 stopifnot(identical(nullGrob(name = "3"), emptyGrob3))
+
+## test for grid.polylineoffset()
+
+linePath = list(list(x = c(2, 4, 4, 2), y = c(2, 2, 4, 4) - 1.5))
+grobPath <- xyListPath(linePath)
+grid.draw(grid.polylineoffset(grobPath,0.5)) # Not working because it is a path. Any solution?
+grid.draw(grobPath)
+
+grob <- linesGrob(x = c(.2, .4, .4, .2), y = c(.2, .2, .4, .4))
+offsetGrob <- grid.polylineoffset(grob, 0.1, jointype="square", endtype = "opensquare")
+grid.draw(offsetGrob)
+grid.draw(grob)
+
+grob <- linesGrob(x = c(.4, .8, .8, .2, .6), y = c(.3, .3, .8, .8, .6))
+offsetGrob <- grid.polylineoffset(grob, 0.1, jointype="square", endtype = "opensquare")
+grid.draw(offsetGrob)
+grid.draw(grob)
+
+## test for grid.polyoffset()
+grob <- polygonGrob(x = c(.4, .8, .8, .2, .6), y = c(.3, .3, .8, .8, .6))
+offsetGrob <- grid.polyoffset(grob, 0.25, jointype = "round")
+grid.draw(offsetGrob)
+grid.draw(grob)
