a267d78fe1d73f02866aadcea4d0cf07b668cbb7
Jack Wong <jackwong199412@hotmail.com.hk>
2022-03-28 14:24:45 +1300

    Update the test by adding null grob object check

--- tests/test2.R
+++ tests/test2.R
@@ -10,23 +10,29 @@ emptyGrob3 = xyListPolygon(x, name = 3)
 stopifnot(identical(nullGrob(name = "3"), emptyGrob3))
 
 ## test for grid.polylineoffset()
-
+grid.newpage()
 linePath = list(list(x = c(2, 4, 4, 2), y = c(2, 2, 4, 4) - 1.5))
-grobPath <- xyListPath(linePath)
-grid.draw(grid.polylineoffset(grobPath,0.5)) # Not working because it is a path. Any solution?
+grobPath <- xyListLine(linePath)
+grid.draw(grid.polylineoffset(grobPath,0.1, jointype="square", endtype = "opensquare")) # Not working because it is a path. Any solution?
 grid.draw(grobPath)
 
-grob <- linesGrob(x = c(.2, .4, .4, .2), y = c(.2, .2, .4, .4))
+grid.newpage()
+grob <- polylineGrob(x = c(2, 4, 4, 2), y = c(0.5, 0.5, 2.5, 2.5), default.units = "in")
 offsetGrob <- grid.polylineoffset(grob, 0.1, jointype="square", endtype = "opensquare")
 grid.draw(offsetGrob)
 grid.draw(grob)
 
+grid.newpage()
 grob <- linesGrob(x = c(.4, .8, .8, .2, .6), y = c(.3, .3, .8, .8, .6))
 offsetGrob <- grid.polylineoffset(grob, 0.1, jointype="square", endtype = "opensquare")
 grid.draw(offsetGrob)
 grid.draw(grob)
 
+##Pass line grob to polyOffset. Expect null grob obj.
+offsetGrob <- grid.polyoffset(grob, 0.1, jointype = "square", endtype = "opensquare")
+
 ## test for grid.polyoffset()
+grid.newpage()
 grob <- polygonGrob(x = c(.4, .8, .8, .2, .6), y = c(.3, .3, .8, .8, .6))
 offsetGrob <- grid.polyoffset(grob, 0.25, jointype = "round")
 grid.draw(offsetGrob)
